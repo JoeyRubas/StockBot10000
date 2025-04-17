a1d9345a58475fce86e265878e1e857fd856e10e
rythama <rythama@gmail.com>
2025-04-17 00:25:33 -0400

    Fix sys.path to make portfolioapp discoverable

--- djangoPortfolio/djangoPortfolio/wsgi.py
+++ djangoPortfolio/djangoPortfolio/wsgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoPortfolio.settings")
